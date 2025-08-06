8624234eb47c854b9c37cdb1b526fe86e9edd963
Puja-main <puja.ahuja@e3l.co>
2025-08-06 12:46:41 +0530

    Add reverse array function

---
+++ reverse_array.py
@@ -0,0 +1,8 @@
+def reverse_array(arr):
+    return arr[::-1]
+
+# Example usage
+if __name__ == '__main__':
+    example_array = [1, 2, 3, 4, 5]
+    reversed_array = reverse_array(example_array)
+    print(reversed_array)  # Output: [5, 4, 3, 2, 1]
